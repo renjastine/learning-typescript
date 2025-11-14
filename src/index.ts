9ed8f04166e8704b2160d5f4996470a5c1a989d0
ren jastine <rnjstntmj@gmail.com>
2025-11-14 12:42:32 +0800

    Union Types

--- src/index.ts
+++ src/index.ts
@@ -33,3 +33,13 @@ let employee: Employee = {
 }
 
 console.log(employee.name);
+
+// Union
+function kgToLbs(weight: number | string): number {
+    // Narrowing
+    if (typeof weight === "number") return weight * 2.2
+    else return parseInt(weight) * 2.2;
+}
+
+kgToLbs(10)
+kgToLbs("10")
