87fbf5e62f6a1d81aaf55bb82789fde040689bb6
ren jastine <rnjstntmj@gmail.com>
2025-11-14 12:14:51 +0800

    Object

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
+// Tuple
 let user: [number, string] = [1, "Ren"];
 console.log(user)
 
+
+// Enum
 const enum Size { Small = 1, Medium, Large }
 let mySize: Size = Size.Medium;
 console.log(mySize)
 
-function CalculateTax (income: number, taxYear= 2022) : number {
+// Function
+function CalculateTax(income: number, taxYear = 2022): number {
     if (taxYear < 2022) return income * 1.2;
     return income * 1.3;
 }
 
 const calculateTax = CalculateTax(10_000, 2055);
 console.log(calculateTax);
+
+// Object
+let employee: {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} = {
+    id: 1,
+    name: "Ren",
+    retire: (date: Date) => {
+        console.log(date);
+    }
+}
+
+console.log(employee.name);
