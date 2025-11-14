4f4b77ed10d976141ac8e602bca3cd9d020c0da8
ren jastine <rnjstntmj@gmail.com>
2025-11-14 08:54:23 +0800

    Functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 let user: [number, string] = [1, "Ren"];
+console.log(user)
 
 const enum Size { Small = 1, Medium, Large }
 let mySize: Size = Size.Medium;
-console.log("")
+console.log(mySize)
+
+function CalculateTax (income: number, taxYear= 2022) : number {
+    if (taxYear < 2022) return income * 1.2;
+    return income * 1.3;
+}
+
+const calculateTax = CalculateTax(10_000, 2055);
+console.log(calculateTax);
